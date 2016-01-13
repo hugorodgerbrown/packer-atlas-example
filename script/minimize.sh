eba561e327e92f052ab9d7a33ac07735aafaf82d
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-01-13 18:45:46 +0000

    Working

--- script/minimize.sh
+++ script/minimize.sh
@@ -6,10 +6,10 @@
 # Remove some packages to get a minimal install
 echo "==> Removing all linux kernels except the currrent one"
 dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v $(uname -r) | xargs apt-get -y purge
-# echo "==> Removing linux source"
-# dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
-# echo "==> Removing development packages"
-# dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
+echo "==> Removing linux source"
+dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
+echo "==> Removing development packages"
+dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
 echo "==> Removing documentation"
 dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
 # echo "==> Removing development tools"
